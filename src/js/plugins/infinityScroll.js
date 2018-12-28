d581238afe48e8896f45e11f6f07d0de1f7d10fa
murilokaos <oofleaoo@gmail.com>
2018-12-28 18:41:14 -0200

    rebuild

---
+++ src/js/plugins/infinityScroll.js
@@ -0,0 +1,22 @@
+import $ from 'jquery'
+import './categorySelect'
+
+export function jScroll() {
+
+    $('.load-more').click(e => {
+        $('.load-more').addClass('fa-spin')
+        setTimeout(() => {
+            $('.load-more').removeClass('fa-spin')
+            $("div[hide|='true']").each((i, e) => {
+                if (i < $('[wm-cursos]').attr('limit')) {
+                    $(e).removeClass('d-none')
+                    $(e).removeAttr('hide')
+                    $(e).parent().removeAttr('hide')
+                }
+            })
+            $('.d-none').each(e => { $(e) === null ? $('.load-more').addClass('d-none') : ''})
+            $('[wm-category-select]').categorySelect()
+        }, 500)
+    })
+}
+
