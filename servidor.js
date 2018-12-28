9bad3f7b7a82b8eeebcd9baf2f055e891064a47f
Murilo Henrique <oofleaoo@gmail.com>
2018-12-28 11:30:18 -0200

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -3,9 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile('index.html');
 })
 
 app.listen(porta, () => {
