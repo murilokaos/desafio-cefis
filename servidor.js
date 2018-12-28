32561489e30cb9c563e1858d64d659847ba130a2
murilokaos <oofleaoo@gmail.com>
2018-12-28 06:07:03 -0200

    build

---
+++ servidor.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(porta, () => {
+    console.log(`O servidor está rodando na porta ${NODE_ENV.port}.`)
+})
